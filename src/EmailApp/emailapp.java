d89400ea9c7469c78a2a57b0eb636a098418dfb9
Clara Sousa Neves <cneves029@gmail.com>
2020-11-14 00:07:45 +0000

    Email Application

---
+++ src/EmailApp/emailapp.java
@@ -0,0 +1,11 @@
+package EmailApp;
+
+public class emailapp {
+
+	public static void main(String[] args) {
+		email em1 = new email("Clara", "Neves");
+		System.out.println(em1.showInfo());
+
+	}
+
+}
